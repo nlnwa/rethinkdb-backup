c95dcd0490d913db750fff38badb5e7b00265e7f
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-02-27 10:14:34 +0100

    refactor: be explicit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,14 @@ eval 'ARGUMENTS="$@"'
 
 case $1 in
     dump)
-        TIMESTAMP=$(date --utc +${DATE_FORMAT:-%Y%m%dT%H%M%SZ})
-        FILE=${FILE:-~+/${FILE_PREFIX:-rethinkdb_dump}_${TIMESTAMP}.${SUFFIX:-tar.gz}}
-        exec rethinkdb $ARGUMENTS --file ${FILE} --temp-dir ${TEMP_DIR:-~+}
+        TEMP_DIR=${TEMP_DIR:-$(pwd)}
+        DATE_FORMAT=${DATE_FORMAT:-%Y%m%dT%H%M%SZ}
+        TIMESTAMP=$(date --utc +${DATE_FORMAT})
+        FILE_SUFFIX=${FILE_SUFFIX:-tar.gz}
+        FILE_PREFIX=${FILE_PREFIX:-rethinkdb_dump}
+        FILE=${FILE:-$(pwd)/${FILE_PREFIX}_${TIMESTAMP}.${FILE_SUFFIX}}
+
+        exec rethinkdb ${ARGUMENTS:---help} --file ${FILE} --temp-dir ${TEMP_DIR}
         ;;
     *)
         exec rethinkdb ${ARGUMENTS:---help}
