c247d758ffaa6bec5997e6e1e3fb4e4dbd2c9b65
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-02-27 01:11:21 +0100

    refactor: entrypoints script evaluates arguments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-exec rethinkdb $@
+eval 'ARGUMENTS="$@"'
+
+case $1 in
+    dump)
+        TIMESTAMP=$(date --utc +${DATE_FORMAT:-%Y%m%dT%H%M%SZ})
+        FILE=${FILE:-~+/${FILE_PREFIX:-rethinkdb_dump}_${TIMESTAMP}.${SUFFIX:-tar.gz}}
+        exec rethinkdb $ARGUMENTS --file ${FILE} --temp-dir ${TEMP_DIR:-~+}
+        ;;
+    *)
+        exec rethinkdb ${ARGUMENTS:---help}
+        ;;
+esac
